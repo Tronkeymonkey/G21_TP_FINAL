0bd09763deec214fe5195f281e1d7e0f686155b1
Tronkeymonkey <tomastroncoso06@gmail.com>
2025-05-05 13:21:40 -0300

    Prueba 1,2,3 a pedido de Sol

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Donantes import *
 from INCUCAI import *
 
 paciente1 = Receptores("Juan", 12345678, "M", "1990-01-01", "O+", 123456789, "Hospital Central", "Riñón", "2023-10-01", 1, "Insuficiencia renal")
+paciente2 = Receptores("Maria", 87654321, "F", "1985-05-15", "A+", 987654321, "Hospital Norte", "Corazón", "2023-10-02", 2, "Cardiopatía")
 
 incucai = INCUCAI()
 
