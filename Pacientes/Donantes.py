ca3055c00fe44d0b5d4e0846cb76b86dd4033dd2
Tronkeymonkey <tomastroncoso06@gmail.com>
2025-05-19 14:32:36 -0300

    Cambio donantes

--- Pacientes/Donantes.py
+++ Pacientes/Donantes.py
@@ -1,5 +1,6 @@
 from pacientes.Pacientes import Pacientes
 from datetime import datetime
+from organos.Organos import Organos
 class Donantes(Pacientes):
 
     def __init__(self, nombre, DNI, sexo, nacimiento, Tsangre, telefono, centro_de_salud, fhfallecimiento, fhablacion):
@@ -7,4 +8,4 @@ class Donantes(Pacientes):
         super().__init__(nombre, DNI, sexo, nacimiento, Tsangre, telefono, centro_de_salud)
         self.fhfallecimiento = fhfallecimiento
         self.fhablacion = fhablacion # lo dejo sin valor inicial (none) hasta que se haga la ablacion 
-        self.organos_a_donar = [] #lista (vacia) de los organos a donar
+        self.organos_a_donar: list[Organos] = [] #lista (vacia) de los organos a donar
